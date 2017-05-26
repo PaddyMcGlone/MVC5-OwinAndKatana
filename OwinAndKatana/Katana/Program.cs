443f9677bac5c235593ccadd61222609f3c56442
Paddy Mc Glone <patrick.mcglone@civica.co.uk>
2017-05-26 16:12:06 +0100

    Adding comments missed in last commit, silly me

--- OwinAndKatana/Katana/Program.cs
+++ OwinAndKatana/Katana/Program.cs
@@ -3,6 +3,7 @@ using Owin;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Katana
@@ -32,14 +33,21 @@ namespace Katana
             // IAppBuilder is important - defines how an application is going to behave and respond to http requests
             public void Configuration(IAppBuilder app)
             {
+                //The two examples below are wrappers for components which can use AppFunc to do some work.
+
                 // Add new page via the addition of diagnostics package
-                app.UseWelcomePage();
+                //app.UseWelcomePage();
 
                 // Run method - katana calls into to process all http requests
                 //app.Run(ctx => ctx.Response.WriteAsync("Hello World")); //WriteAsync, this is a method which returns a task.
+
+                //Register low level component
+                AppBuilderUseExtensions.Use<HelloWorldComponent>();
             }
         }
 
+
+        // Low level code you can write
         public class HelloWorldComponent
         {
 
@@ -47,12 +55,22 @@ namespace Katana
             AppFunc _next;
             public HelloWorldComponent(AppFunc nextComponent)
             {
-                _next = nextComponent;
+                _next = nextComponent; //Give me the next component in the pipeline
             }
 
-            public async Task Invoke(IDictionary<string, object> enviornment)
+            // Katana finds this via reflection which matches the AppFunc signature.
+            public Task Invoke(IDictionary<string, object> enviornment)
             {
-                await _next();
+                // Invoke next item on the chain and pass on the dictionary
+                //await _next(enviornment);
+
+                // In the pipeline if we want to end everything and send something back to the 
+                // client we can do this with the responsebody component.
+                var response = enviornment["owin.ResponseBody"] as Stream;
+                using (var writer = new StreamWriter(response))
+                {
+                    return writer.WriteAsync("Hello!!");
+                }
             }
         }
 
