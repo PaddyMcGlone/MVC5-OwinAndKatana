a21718728b4722225f86c055b3c349bdd92cebfb
Paddy Mc Glone <patrick.mcglone@civica.co.uk>
2017-05-26 15:35:44 +0100

    Adding comments to my code

--- OwinAndKatana/Katana/Program.cs
+++ OwinAndKatana/Katana/Program.cs
@@ -10,9 +10,9 @@ namespace Katana
         static void Main(string[] args)
         {
             //Start the server
-            string uri = "http://localhost:58146";
+            string uri = "http://localhost:8080";
 
-            using (WebApp.Start<Startup>(uri))
+            using (WebApp.Start<Startup>(uri)) // Explicitly starting a web server (Uri listen at given location)
             {
                 Console.WriteLine("Started!");
                 Console.ReadKey();
@@ -20,13 +20,17 @@ namespace Katana
             }
         }
 
+        // This method tells the server how to configure itself.
         public class Startup
         {
             // IAppBuilder is important - defines how an application is going to behave and respond to http requests
             public void Configuration(IAppBuilder app)
             {
+                // Add new page via the addition of diagnostics package
+                app.UseWelcomePage();
+
                 // Run method - katana calls into to process all http requests
-                app.Run(ctx => ctx.Response.WriteAsync("Hello World")); //WriteAsync, this is a method which returns a task.
+                //app.Run(ctx => ctx.Response.WriteAsync("Hello World")); //WriteAsync, this is a method which returns a task.
             }
         }
 
