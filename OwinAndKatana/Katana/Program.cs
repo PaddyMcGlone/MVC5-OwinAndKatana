8bc87180107a82cb702d8c889bc1331d56f822d9
Paddy Mc Glone <patrick.mcglone@civica.co.uk>
2017-05-26 15:28:43 +0100

    Adding Welcome page and inital server startup code

--- OwinAndKatana/Katana/Program.cs
+++ OwinAndKatana/Katana/Program.cs
@@ -1,15 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
 
 namespace Katana
 {
     class Program
     {
+        // Entry point
         static void Main(string[] args)
         {
+            //Start the server
+            string uri = "http://localhost:58146";
+
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Started!");
+                Console.ReadKey();
+                Console.WriteLine("Stopping");
+            }
         }
+
+        public class Startup
+        {
+            // IAppBuilder is important - defines how an application is going to behave and respond to http requests
+            public void Configuration(IAppBuilder app)
+            {
+                // Run method - katana calls into to process all http requests
+                app.Run(ctx => ctx.Response.WriteAsync("Hello World")); //WriteAsync, this is a method which returns a task.
+            }
+        }
+
     }
 }
